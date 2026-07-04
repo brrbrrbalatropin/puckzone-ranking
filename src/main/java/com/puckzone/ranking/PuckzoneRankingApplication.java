bbc41d913c423ae26dbba96de4b7d7c4451618e0
Daniel Felipe Rayo Rodriguez <daniel.rayo@mail.escuelaing.edu.co>
2026-07-03 19:52:06 -0500

    first commit

---
+++ src/main/java/com/puckzone/ranking/PuckzoneRankingApplication.java
@@ -0,0 +1,13 @@
+package com.puckzone.ranking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuckzoneRankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuckzoneRankingApplication.class, args);
+	}
+
+}
